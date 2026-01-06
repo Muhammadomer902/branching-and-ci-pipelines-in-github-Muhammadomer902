662f96168002b78a7805359e64c9c83bffe4cac9
Muhammadomer902 <sayyidmuhammadomer@gmail.com>
2026-01-06 20:28:42 +0500

    Fixed test assertion

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,5 +5,5 @@ def test_about_page():
     # Use Flask's test client
     client = app.test_client()
     resp = client.get("/about")
-    assert resp.status_code == 404
+    assert resp.status_code == 200
     assert b"About This App" in resp.data
